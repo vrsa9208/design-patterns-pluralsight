40cd4c978a1789246c1ff69d7a93cf9eee56633b
vvo0002 <vvo0002@mail.wal-mart.com>
2019-02-05 14:50:40 -0600

    Add thread safety to singleton

--- src/main/java/com/vrsa9208/designPatterns/creational/singleton/DbSingleton.java
+++ src/main/java/com/vrsa9208/designPatterns/creational/singleton/DbSingleton.java
@@ -2,13 +2,22 @@ package com.vrsa9208.designPatterns.creational.singleton;
 
 public class DbSingleton {
 
-	private static DbSingleton instance = null;
+	private static volatile DbSingleton instance = null;
 	
-	private DbSingleton() {}
+	private DbSingleton() {
+		// This validation is for instances using reflection
+		if(instance != null) {
+			throw new RuntimeException("Use getInstance() method to create");
+		}
+	}
 	
 	public static DbSingleton getInstance() {
 		if(instance == null) {
-			 instance = new DbSingleton();
+			synchronized(DbSingleton.class) {	
+				if(instance == null) {					
+					instance = new DbSingleton();
+				}
+			}
 		}
 		
 		return instance;
