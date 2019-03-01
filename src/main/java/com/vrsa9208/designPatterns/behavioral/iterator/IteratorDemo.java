446c3050351e91d53a7bf3968f5f4825ac0acd1a
Victor Santiago <vrsa9208@gmail.com>
2019-03-01 10:52:55 -0600

    Iterator pattern

---
+++ src/main/java/com/vrsa9208/designPatterns/behavioral/iterator/IteratorDemo.java
@@ -0,0 +1,24 @@
+package com.vrsa9208.designPatterns.behavioral.iterator;
+
+import java.util.Iterator;
+
+public class IteratorDemo {
+
+	public static void main(String[] args) {
+		BikeRepository repo = new BikeRepository();
+		
+		repo.addBike("Cervelo");
+		repo.addBike("Scott");
+		repo.addBike("Fuji");
+		
+		Iterator<String> bikeIterator = repo.iterator();
+		
+		while(bikeIterator.hasNext()) {
+			System.out.println(bikeIterator.next());
+		}
+		
+		for (String bike : repo) {
+			System.out.println(bike);
+		}
+	}
+}
