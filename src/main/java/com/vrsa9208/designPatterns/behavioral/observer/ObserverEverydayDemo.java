08271a88ed9980d8a57bee724c043d494e927895
Victor Santiago <vrsa9208@gmail.com>
2019-03-20 11:42:36 -0600

    Observer everyday demo

---
+++ src/main/java/com/vrsa9208/designPatterns/behavioral/observer/ObserverEverydayDemo.java
@@ -0,0 +1,41 @@
+package com.vrsa9208.designPatterns.behavioral.observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ObserverEverydayDemo {
+
+	public static void main(String[] args) {
+		TwitterStream messageStream = new TwitterStream();
+
+		Client client1 = new Client("Bryan");
+		Client client2 = new Client("Mark");
+
+		messageStream.addObserver(client1);
+		messageStream.addObserver(client2);
+
+		messageStream.someoneTweeted();
+	}
+}
+
+class TwitterStream extends Observable {
+	
+	public void someoneTweeted() {
+		setChanged();
+		notifyObservers();
+	}
+}
+
+class Client implements Observer {
+	
+	private String name;
+	
+	public Client(String name){
+		this.name = name;
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		System.out.println("Update " + name + "'s stream, someone tweeted something");
+	}
+}
